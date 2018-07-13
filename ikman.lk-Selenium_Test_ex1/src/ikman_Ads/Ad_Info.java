9857b54147ece0e238d9ae24ce47a9c03a5d246a
EGRaY5 <erandagrero@gmail.com>
2018-07-13 20:25:35 +0530

    reinitialized files

---
+++ ikman.lk-Selenium_Test_ex1/src/ikman_Ads/Ad_Info.java
@@ -0,0 +1,12 @@
+package ikman_Ads;
+
+public class Ad_Info {
+
+    public int noOfBeds;
+    public int price;
+
+    public Ad_Info(int beds, int price) {
+        noOfBeds = beds;
+        price = price;
+    }
+}
